8e8f0f4d0c3350f0771d6a36b8aeefd29ec45b6d
Maite <maitecondepalacio@gmail.com>
2020-06-06 14:08:34 +0200

    disabled dates

--- frontend/src/containers/room/Calendar.jsx
+++ frontend/src/containers/room/Calendar.jsx
@@ -8,9 +8,14 @@ import {getAllReservations} from '../../redux/actions/reservations'
 
 import {reservDate} from '../../redux/actions/rooms'
 
-getAllReservations()
+
 
 class ReservationCalendar extends React.Component {
+  componentDidMount(props){
+    getAllReservations()
+  }
+
+
   state = {
     value: moment(),
     selectedValue: moment(),
@@ -33,17 +38,22 @@ class ReservationCalendar extends React.Component {
     const { value, selectedValue } = this.state;
     reservDate(selectedValue)
 
+   const dates = this.props.reservations.map((res => res.date))
    
     return (
       <div>
+  
+ <Calendar value={value} onSelect={this.onSelect} onPanelChange={this.onPanelChange}
+       disabledDate={ current => {
+         
+        let index = dates.findIndex(date => date === moment(current).format('YYYY-MM-DD'))
+        return index !== -1 && true
 
-        {/* {reservations?.map((res=>res.date))} */}
-        <Calendar value={value} onSelect={this.onSelect} onPanelChange={this.onPanelChange}
-         disabledDate={current => {
-           return current && current < moment('2020-06-07') ;
-        }}
-        />
+       }
+       }
+      />
 
+    
       </div>
     );
   }
